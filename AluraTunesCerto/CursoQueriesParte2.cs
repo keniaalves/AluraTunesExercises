d39f9cc324077a86bfda6b8bcd5d02756f93ae28
Kênia Alves <kenia.alves@enviou.com.br>
2022-07-13 08:03:52 -0300

    Starting course 2 - Pagination

---
+++ AluraTunesCerto/CursoQueriesParte2.cs
@@ -0,0 +1,43 @@
+﻿using AluraTunesCerto.DataSQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace AluraTunesCerto
+{
+	internal class CursoQueriesParte2
+	{
+		internal static void ListarVendas()
+		{
+			using (var contexto = new AluraTunesEntities())
+			{
+				var query = from nf in contexto.NotaFiscals
+							orderby nf.NotaFiscalId ascending
+
+							select new
+							{
+								Numero = nf.NotaFiscalId,
+								Data = nf.DataNotaFiscal,
+								Cliente = nf.Cliente.PrimeiroNome + " " + nf.Cliente.Sobrenome,
+								Total = nf.Total
+							};
+				const int ENTRIES = 100;
+				var todasNFs = query.Count();
+				
+				for (int i = 0; ENTRIES * i <= todasNFs; i++)
+				{ 
+					var pulos = (i) * ENTRIES;
+					var dadosPaginados = query.Skip(pulos).Take(ENTRIES);
+					Console.WriteLine(" ");
+					Console.WriteLine("------------------------");
+					Console.WriteLine("Página" + 1 + i);
+					Console.WriteLine(" ");
+					foreach (var nf in dadosPaginados)
+					{
+						Console.WriteLine($"{nf.Numero}, {nf.Data}, {nf.Cliente}, {nf.Total}");
+					}
+				}
+				Console.ReadKey();
+			}
+		}
+	}
+}
